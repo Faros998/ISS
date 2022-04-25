3380e359e11fd066a40a37c4eb631ad3695b6873
Faros <99401477+Faros998@users.noreply.github.com>
2022-04-25 15:06:10 +0200

    Delete web15a directory

--- web15a/JS/time.js
+++
@@ -1,7 +0,0 @@
-function naplnCas (){
-    var datum = new Date(); 
-    aktualniCas = datum.getHours() + ":" + datum.getMinutes() + ":" + datum.getSeconds();  
-    window.document.getElementById("cas").innerHTML = aktualniCas;
-    }  
-    naplnCas(); 
-    window.setInterval("naplnCas()", 1000); 
